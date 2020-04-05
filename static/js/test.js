8dc713675bbb4cd1a1b509ee34d05a754271b963
HadesBob <lukas.goinski@gmail.com>
2020-04-05 03:09:45 +0200

    change in animation

--- static/js/test.js
+++ static/js/test.js
@@ -17,10 +17,57 @@ gsap.registerPlugin(MotionPathPlugin, TextPlugin, Draggable);
 	var leftRocket = $('.rocket-left');
 	var rightRocket = $('.rocket-right');
 	var letter = document.querySelectorAll('.letter-span');
-	var isTilesHide = true;
+	var isTilesHide = false;
+function initTiles(){
+			var tilesWrapper = $('.tiles-container');	
+			var tilesWrapperWidth = tilesWrapper.width();
+			var tilesWrapperHeight = tilesWrapper.height();
+			for(var i=0; i<56; i++){
+				var tile = $(document.createElement('div')).addClass('tile');
+				tile.css({"width": tilesWrapperWidth/8, "height": tilesWrapperHeight/8, "backgroundColor":"grey" });
+				tilesWrapper.append(tile);
+			}
+			
+			}
 
 
+	initTiles();
 
+
+function initContent(id, iconUrl, title){
+	$.getJSON('/static/json/description.json', function(data) {
+             var x = findByName(data, id);
+             var result = data.data;
+               })
+	var tilesContentWrapper = $('.content-wrapper');	
+	var contentBox = $(document.createElement('div')).addClass('content-box').attr('id',id);
+	tilesContentWrapper.append(contentBox);
+	var tilesContentHeader = $(document.createElement('div')).addClass('content-header');
+	contentBox.append(tilesContentHeader);
+	var icon = $(document.createElement('img')).addClass('content-icon').attr('src','/static/photos/' + iconUrl);
+	var title = $(document.createElement('h1')).addClass('content-title').text(title);
+	var closeIcon = $(document.createElement('img')).addClass('close-icon').attr('src','/static/photos/close4.png').attr('value', id).attr('id', 'close-icon');
+	tilesContentHeader.append(icon, title, closeIcon);
+	var tilesContent = $(document.createElement('div')).addClass('content');
+	contentBox.append(tilesContent);
+	
+		closeIcon.click(function(){
+			var hide = gsap.timeline();
+				hide.to(wheelContent, 0.5, {scale: 0.7,y:0},).to('.tile',0.5, {
+					scale:0,
+					y:80,
+					opacity: 0,
+					stagger:{
+						amount:1,
+						axis: "x",
+						ease: Power3.easeOut
+					}
+				}, "-=1").to('#' + id, 0.5,{opacity: 0, filter:"blur(20px)"}, "-=1");
+				isTilesHide = true;
+		})
+	}
+
+	initContent("start-content", "start-icon.png", "HELLO WORLD");
 	var tl = gsap.timeline();
 	tl.from(letter, 1, {opacity: 0, filter:"blur(160px)", stagger: .25, ease:"ease-out"})
 	.to(letter,1, {stagger:.01, color: "white", ease:"ease-out",textShadow:"0px 5px 10px white"})
@@ -31,7 +78,16 @@ gsap.registerPlugin(MotionPathPlugin, TextPlugin, Draggable);
 	
 		tl2 = gsap.timeline({delay: 13});
 		tl2.to(wheelWrapper,1,{visibility: "transparent"})
-			.to(wheelContent,3,{scale:0.7, marginLeft:"600px", paddingTop: "500px", complete:removeVideoWrapper}, "-=1");
+			.to(wheelContent,2,{scale:0.7, marginLeft:"600px", paddingTop: "500px", complete:removeVideoWrapper}, "-=1")
+			.to('#start-content', 1, {opacity:1, filter:"blur(0px)"}).to('.tile', 1, {
+					scale:1,
+					y:0,
+					opacity: 0.3,
+					stagger:{
+						amount:1,
+						ease: Power3.linear
+					}
+				});
 
 			tl3 = gsap.timeline();
 			tl3.fromTo(wheelCarousel,5,{rotateY:360},{rotateY:0,repeat: -1, ease: "linear"})
@@ -62,19 +118,7 @@ gsap.registerPlugin(MotionPathPlugin, TextPlugin, Draggable);
 	})
 
 
-	function startWheel(){
-		setTimeout(function(){
-			tl3.resume(); 
-		}, 5000);
-	}
-	function removeVideoWrapper(){
-		wrapper.remove();
-	}
-	function removeVideoPart(){
-		$('.video').remove();
-		$('.letter-span-wrapper').remove();
-	}
-
+	
 
 	function generateLettersArray(string){
 		var text = string;
@@ -102,64 +146,11 @@ gsap.registerPlugin(MotionPathPlugin, TextPlugin, Draggable);
 		
 	}
 
-	function initTiles(){
-			var tilesWrapper = $('.tiles-container');	
-			var tilesWrapperWidth = tilesWrapper.width();
-			var tilesWrapperHeight = tilesWrapper.height();
-			for(var i=0; i<56; i++){
-				var tile = $(document.createElement('div')).addClass('tile');
-				tile.css({"width": tilesWrapperWidth/8, "height": tilesWrapperHeight/8, "backgroundColor":"grey" });
-				tilesWrapper.append(tile);
-			}
-			
-			}
-
-
-	initTiles();
 	
-	function findByName(array, name){
-		for(var i=1; i<array.data.length; i++){
-			if(array.data[i].name === name){
-				return array.data[i].skills;
-			}
-			else{
-				return "Kupsko";
-			}
-		}
-	}
-
-	function initContent(id, iconUrl, title){
-	$.getJSON('/static/json/description.json', function(data) {
-             var x = findByName(data, id);
-             var result = data.data;
-               })
-	var tilesContentWrapper = $('.content-wrapper');	
-	var contentBox = $(document.createElement('div')).addClass('content-box').attr('id',id);
-	tilesContentWrapper.append(contentBox);
-	var tilesContentHeader = $(document.createElement('div')).addClass('content-header');
-	contentBox.append(tilesContentHeader);
-	var icon = $(document.createElement('img')).addClass('content-icon').attr('src','/static/photos/' + iconUrl);
-	var title = $(document.createElement('h1')).addClass('content-title').text(title);
-	var closeIcon = $(document.createElement('img')).addClass('close-icon').attr('src','/static/photos/close4.png').attr('value', id).attr('id', 'close-icon');
-	tilesContentHeader.append(icon, title, closeIcon);
-	var tilesContent = $(document.createElement('div')).addClass('content');
-	contentBox.append(tilesContent);
 	
-		closeIcon.click(function(){
-			var hide = gsap.timeline();
-				hide.to(wheelContent, 1, {scale: 0.7,y:0}).to('.tile', 1, {
-					scale:0,
-					y:80,
-					opacity: 0,
-					stagger:{
-						amount:1,
-						axis: "x",
-						ease: Power3.easeOut
-					}
-				}, "-=1").to('#' + id, 1,{opacity: 0});
-				isTilesHide = true;
-		})
-	}
+
+
+	
 
 
 	$('.wheel-carrousel-item').click(function(e){
@@ -238,13 +229,43 @@ gsap.registerPlugin(MotionPathPlugin, TextPlugin, Draggable);
 						axis: "x",
 						ease: Power3.easeOut
 					}
-				}, "-=1").to('#' + id, 1,{opacity: 1});
+				}, "-=1").to('#' + id, 1,{opacity: 1, filter:"blur(0px)"});
 			isTilesHide = false;
 		}else{
 			return;
 		}	
 	});
 
-	
+
+
+
+
+
+
+
+
+function startWheel(){
+		setTimeout(function(){
+			tl3.resume(); 
+		}, 5000);
+	}
+	function removeVideoWrapper(){
+		wrapper.remove();
+	}
+	function removeVideoPart(){
+		$('.video').remove();
+		$('.letter-span-wrapper').remove();
+	}
+
+		function findByName(array, name){
+		for(var i=1; i<array.data.length; i++){
+			if(array.data[i].name === name){
+				return array.data[i].skills;
+			}
+			else{
+				return "Kupsko";
+			}
+		}
+	}
 });
 
